87be25835ce27f5d06ad9d5f6d73f309159ced31
Luiz Fernando Zamboti <dev.zamboti@gmail.com>
2024-04-01 16:23:16 -0300

    Adicionar arquivos de projeto.

---
+++ Grupo 4/TabuadaEstatica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo4
+{
+    public static class TabuadaEstatica
+    {
+        public static void Calcular(int valor)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{valor} * {i} = {valor * i}");
+            }
+        }
+    }
+}
